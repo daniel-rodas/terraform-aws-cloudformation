5bd58244c9f1e64a8ce3fd67d7d7151f9a7451d0
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-02-16 20:16:09 -0800

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -1,20 +1,20 @@
-module "stack_set_example_1" {
-  source = "../"
+# module "stack_set_example_1" {
+#   source = "../"
 
-  # stackset_parameters_map = {
-  #   # UsernameList = "[\"bob\", \"alice\"]"
-  #   IAMUser = "alice"
-  # }
+#   # stackset_parameters_map = {
+#   #   # UsernameList = "[\"bob\", \"alice\"]"
+#   #   IAMUser = "alice"
+#   # }
 
-  stackset_name = "Test1"
-  stackset_template_body       = data.local_file.template.content
+#   stackset_name = "Test1"
+#   stackset_template_body       = data.local_file.template.content
 
-  stacksetinstance_deployment_targets = {
-    # organizational_unit_ids = [data.aws_organizations_organization.organization.accounts[2].id]
-    # organizational_unit_ids = [data.aws_organizations_organization.organization.roots[0].id]
-    organizational_unit_ids = ["ou-y0nu-rs58fwa3", "ou-y0nu-ofdu49ji"]
-  }
-}
+#   stacksetinstance_deployment_targets = {
+#     # organizational_unit_ids = [data.aws_organizations_organization.organization.accounts[2].id]
+#     # organizational_unit_ids = [data.aws_organizations_organization.organization.roots[0].id]
+#     organizational_unit_ids = ["ou-y0nu-rs58fwa3", "ou-y0nu-ofdu49ji"]
+#   }
+# }
 
 data "local_file" "template" {
   filename = "./templates/cloudformation/assume-role.template.json"
